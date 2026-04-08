455f455d781119cf6c4af12d02343f520c698282
iFamishedX <me@ifamished.com>
2026-04-08 20:46:33 +0000

    Fix imports (yet again)

--- src/hungerlib/__init__.py
+++ src/hungerlib/__init__.py
@@ -1,12 +1,37 @@
-from .servers import MinecraftServer
+# Re-export modules
+from . import utils
+from . import scheduler
+from . import logger
+from . import config
+from . import colormap
+from . import mchelpers
+
+# Re-export classes
 from .panel import Panel
-from .mchelpers import MCHelpers
-from .config import DefaultConfig
-from .utils import *
+from .servers import GenericServer, MinecraftServer
+
+# Re-export selected functions
+try:
+    from .utils import checklag
+except ImportError:
+    pass
+
+# Version (optional)
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "0.0.0"
 
 __all__ = [
-    "MinecraftServer",
+    "utils",
+    "scheduler",
+    "logger",
+    "config",
+    "colormap",
+    "mchelpers",
     "Panel",
-    "MCHelpers",
-    "DefaultConfig",
+    "GenericServer",
+    "MinecraftServer",
+    "checklag",
+    "__version__",
 ]
