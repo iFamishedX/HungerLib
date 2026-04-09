ff83427e4465423dbd5ed78ddc4bb4466276eb49
iFamishedX <me@ifamished.com>
2026-04-09 02:56:53 +0000

    Rewrite __init__.py

--- src/hungerlib/__init__.py
+++ src/hungerlib/__init__.py
@@ -1,29 +1,4 @@
-# Re-export modules
-from . import utils
-from . import scheduler
-from . import logger
-from . import config
-from . import colormap
-from . import mchelpers
-
-# Re-export classes from the new servers/ folder
-from .servers._generic import GenericServer
-from .servers.minecraft import MinecraftServer
-
-# Re-export Panel
-from .panel import Panel
-
-# Re-export selected functions
-try:
-    from .utils import checkLag
-except ImportError:
-    pass
-
-# Version
-try:
-    from ._version import __version__
-except ImportError:
-    __version__ = "0.0.0"
+# HungerLib public API with safe lazy imports
 
 __all__ = [
     "utils",
@@ -35,6 +10,40 @@ __all__ = [
     "Panel",
     "GenericServer",
     "MinecraftServer",
-    "checklag",
-    "__version__",
+    "checkLag",
 ]
+
+# Lazy imports to avoid import-time failures during packaging
+def __getattr__(name):
+    if name == "utils":
+        from . import utils
+        return utils
+    if name == "scheduler":
+        from . import scheduler
+        return scheduler
+    if name == "logger":
+        from . import logger
+        return logger
+    if name == "config":
+        from . import config
+        return config
+    if name == "colormap":
+        from . import colormap
+        return colormap
+    if name == "mchelpers":
+        from . import mchelpers
+        return mchelpers
+    if name == "Panel":
+        from .panel import Panel
+        return Panel
+    if name == "GenericServer":
+        from .servers._generic import GenericServer
+        return GenericServer
+    if name == "MinecraftServer":
+        from .servers.minecraft import MinecraftServer
+        return MinecraftServer
+    if name == "checkLag":
+        from .utils import checkLag
+        return checkLag
+
+    raise AttributeError(f"module 'hungerlib' has no attribute '{name}'")
