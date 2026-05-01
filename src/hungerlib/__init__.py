2ce08f393b949dba7ad78fbe618b83a26bc62578
iFamishedX <me@ifamished.com>
2026-05-01 19:57:35 +0000

    Test init rewrite

--- src/hungerlib/__init__.py
+++ src/hungerlib/__init__.py
@@ -1,3 +1,5 @@
+# hungerlib/__init__.py
+
 from importlib.metadata import version as _pkg_version, PackageNotFoundError
 
 # Package version
@@ -14,11 +16,31 @@ from . import datamap
 from . import configloader
 from . import utils
 
+
+from .datamap import (
+    set_default_maps,
+    get_default_maps,
+    Syntax,
+    DataMap,
+    datamap as datamap_decorator,
+    mapit,
+)
+
+
 __all__ = [
+    # modules
     "panel",
     "servers",
     "messagerouter",
     "datamap",
     "configloader",
     "utils",
+
+    # datamap symbols
+    "set_default_maps",
+    "get_default_maps",
+    "Syntax",
+    "DataMap",
+    "datamap_decorator",
+    "mapit",
 ]
